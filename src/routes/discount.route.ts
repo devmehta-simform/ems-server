34573ca46e87a324040e5ac150ac9fea45be8793
devmehta-simform <dev.mehta@simformsolutions.com>
2025-04-17 16:08:48 +0530

    refactor: remove hard-coded roles and instead add RolesEnum for accessing roles

--- src/routes/discount.route.ts
+++ src/routes/discount.route.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import { auth, validate } from '../middlewares';
 import * as DiscountService from '../services/discount.service';
 import { DiscountCreateSchema, DiscountUpdateStatusSchema } from '../dto';
+import { RolesEnum } from '../types';
 
 const discountRouter = express.Router();
 
-discountRouter.route('/').post(auth('Host'), validate(DiscountCreateSchema), DiscountService.createDiscount);
+discountRouter.route('/').post(auth(RolesEnum['Host']), validate(DiscountCreateSchema), DiscountService.createDiscount);
 
 discountRouter
   .route('/:discountId')
   .get(auth(), DiscountService.getDiscount)
-  .patch(auth('Host'), validate(DiscountUpdateStatusSchema), DiscountService.updateDiscountStatus);
+  .patch(auth(RolesEnum['Host']), validate(DiscountUpdateStatusSchema), DiscountService.updateDiscountStatus);
 
 export { discountRouter };
--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -2,15 +2,16 @@ import express from 'express';
 import { auth, validate } from '../middlewares';
 import * as EventService from '../services/event.service';
 import { EventCreateSchema, EventUpdateSchema } from '../dto';
+import { RolesEnum } from '../types';
 
 const eventRouter = express.Router();
 
-eventRouter.route('/').get(auth(), EventService.getEvents).post(auth('Host'), validate(EventCreateSchema), EventService.createEvent);
+eventRouter.route('/').get(auth(), EventService.getEvents).post(auth(RolesEnum['Host']), validate(EventCreateSchema), EventService.createEvent);
 
 eventRouter
   .route('/:eventId')
   .get(auth(), EventService.getEvent)
-  .patch(auth('Host'), validate(EventUpdateSchema), EventService.updateEvent)
-  .delete(auth('Host'), EventService.deleteEvent);
+  .patch(auth(RolesEnum['Host']), validate(EventUpdateSchema), EventService.updateEvent)
+  .delete(auth(RolesEnum['Host']), EventService.deleteEvent);
 
 export { eventRouter };
