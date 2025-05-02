f334827c0f5786988bb056ca048a17d5c844a9d4
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-02 12:31:45 +0530

    fix: remove images from getEvents response

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -14,7 +14,6 @@ export const getEvents: RequestHandler = RequestHandlerWrapper(async function (_
       zipcode: true,
       coverImage: true,
       dateOfEvent: true,
-      images: true,
       ticketPrice: true,
       createdAt: true,
       reviews: true,
